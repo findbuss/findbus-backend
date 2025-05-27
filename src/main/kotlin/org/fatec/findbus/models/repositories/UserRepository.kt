6309bb1ff0ddd22cd49ec84934522e4b73062550
douglasdans <douglas.dans@gmail.com>
2025-05-27 17:22:06 -0300

    refactor: add optional return value to findByEmail on user.repository

--- src/main/kotlin/org/fatec/findbus/models/repositories/UserRepository.kt
+++ src/main/kotlin/org/fatec/findbus/models/repositories/UserRepository.kt
@@ -3,9 +3,10 @@ package org.fatec.findbus.models.repositories
 import org.fatec.findbus.models.entities.User
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import java.util.Optional
 
 @Repository
 interface UserRepository : JpaRepository<User, Long> {
     fun findByEmail(email: String): User?
-    fun findUserByEmail(email: String): User
+    fun findUserByEmail(email: String): Optional<User>
 }
