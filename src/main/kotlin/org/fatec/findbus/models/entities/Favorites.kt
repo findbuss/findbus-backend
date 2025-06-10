4d0fe2c80ca31b25c43acbea644ac91aced8ddf5
douglasdans <douglas.dans@gmail.com>
2025-06-10 08:14:43 -0300

    feat: update Favorites model to include routeId and change user relationship fetch type

--- src/main/kotlin/org/fatec/findbus/models/entities/Favorites.kt
+++ src/main/kotlin/org/fatec/findbus/models/entities/Favorites.kt
@@ -9,20 +9,23 @@ data class Favorites(
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     val id: Long = 0,
-    
+
     @Column(name = "line_id", nullable = false)
     val lineId: String,
-    
+
+    @Column(name = "route_id", nullable = false)
+    val routeId: String,
+
     @Column(name = "line_name", nullable = false)
     val lineName: String,
-    
+
     @Column(name = "shape_id", nullable = false)
     val shapeId: String,
-    
+
     @Column(name = "updated_at", nullable = false)
     val updatedAt: LocalDateTime = LocalDateTime.now(),
-    
-    @ManyToOne(fetch = FetchType.LAZY)
+
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id", nullable = false)
     val user: User
 )
